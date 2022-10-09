a814c9d2a73727726998ee1b5273ab85be2ab780
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-10-09 12:19:13 +0200

    feat: add class and spell schools icons
    * add svg files for class icons and spell school icons
    * add png files for class icons and spell school icons
    * create temporar layout to check png icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import ClassIcons from 'components/ClassIcons/ClassIcons.json'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '10vh auto',
+      }}
+    >
+      {ClassIcons &&
+        ClassIcons.map((characterClass) => {
+          return (
+            <div
+              key={characterClass.id}
+              style={{
+                color: characterClass.color,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                maxWidth: '100px',
+              }}
+            >
+              <div>
+                <img
+                  style={{
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                  }}
+                  alt={`${characterClass.class} class icon`}
+                  src={`assets/classes/${characterClass.class}.png`}
+                />
+              </div>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
-
 export default App
