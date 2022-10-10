4b5a0c1b2cfa9639dcc30612faae8130a8bce96b
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-10-10 15:24:09 +0200

    feat: add homepage background and navbar
    * add background images for homepage
    * add navbar
    * add navbar color change on scroll
    * set global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import ClassIcons from 'components/ClassIcons/ClassIcons.json'
+import Navbar from 'components/Navbar'
+import Homepage from 'pages/Homepage'
 
 function App() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '10vh auto',
-      }}
-    >
-      {ClassIcons &&
-        ClassIcons.map((characterClass) => {
-          return (
-            <div
-              key={characterClass.id}
-              style={{
-                color: characterClass.color,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                maxWidth: '100px',
-              }}
-            >
-              <div>
-                <img
-                  style={{
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                  }}
-                  alt={`${characterClass.class} class icon`}
-                  src={`assets/classes/${characterClass.class}.png`}
-                />
-              </div>
-            </div>
-          )
-        })}
+    <div>
+      <Navbar />
+      <Homepage />
     </div>
   )
 }
---
+++ src/components/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Aclonica', sans-serif;
+    overflow-x: hidden;
+  }
+`
+
+export default GlobalStyle
