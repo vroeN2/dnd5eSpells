cd9caa043a73ffb5cf97a751afaec66332c157d8
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-10-13 12:16:25 +0200

    feat: add homepage text
    * create heading and description texts for homepage
    * position them according to project file

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,54 +1,5 @@
 import { useEffect, useState } from 'react'
-import styled from 'styled-components'
-
-const NavbarWrapper = styled.div`
-  width: 100vw;
-  height: 10vh;
-  /* background: #ffffff; */
-  background: ${(props) => props.color};
-  position: fixed;
-  top: 0;
-  left: 0;
-  /* color: #1d1d1d; */
-  color: ${(props) => (props.color === '#ffffff' ? '#1d1d1d' : '#ffffff')};
-  text-shadow: ${(props) => (props.color === '#ffffff' ? '0px' : '0px 0px 2px #1d1d1d')};
-  transition: 0.2s all ease;
-  padding: 1vh 2vw;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const LogoWrapper = styled.div`
-  font-size: 3.5rem;
-`
-
-const MenuWrapper = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-`
-
-const MenuItem = styled.button`
-  background-color: transparent;
-  border: none;
-  transition: 0.2s all ease;
-  font-size: 2.5rem;
-
-  &::after {
-    content: '';
-    width: 0px;
-    height: 0.225rem;
-    display: block;
-    background: black;
-    transition: 300ms;
-  }
-
-  &:hover::after {
-    width: 100%;
-  }
-`
+import { LogoWrapper, MenuItem, MenuWrapper, NavbarWrapper } from './styled'
 
 const Navbar = () => {
   const [scrollY, setScrollY] = useState(0)
@@ -77,9 +28,13 @@ const Navbar = () => {
       <LogoWrapper>DnD Spells List</LogoWrapper>
 
       <MenuWrapper style={{ color: 'inherit' }}>
-        <MenuItem style={{ marginRight: '2rem', color: 'inherit' }}>All spells</MenuItem>
+        <MenuItem style={{ marginRight: '2rem', color: 'inherit' }} color={navbarBackgroundColor}>
+          All spells
+        </MenuItem>
 
-        <MenuItem style={{ color: 'inherit' }}>Saved</MenuItem>
+        <MenuItem style={{ color: 'inherit' }} color={navbarBackgroundColor}>
+          Saved
+        </MenuItem>
       </MenuWrapper>
     </NavbarWrapper>
   )
--- src/pages/Homepage/styled.ts
+++ src/pages/Homepage/styled.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const FirstSpacingDiv = styled.div`
+  margin-top: -1vh;
   width: 100%;
   height: 10vh;
-  margin-top: -1vh;
   background: linear-gradient(
     180deg,
     rgba(1, 17, 3, 1) 0%,
@@ -22,4 +22,80 @@ export const SecondSpacingDiv = styled.div`
     rgba(3, 15, 3, 1) 65%,
     rgba(4, 9, 17, 1) 100%
   );
+`
+
+export const SlideWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+`
+
+export const FirstSlideWrapper = styled(SlideWrapper)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const SecondSlideWrapper = styled(SlideWrapper)`
+  display: flex;
+  align-items: center;
+`
+
+export const ThirdSlideWrapper = styled(SlideWrapper)`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+export const BackgroundImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-height: 100%;
+`
+
+export const SecondSlideTextWrapper = styled.div`
+  width: 100%;
+  margin-left: 45vw;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+`
+
+export const ThirdSlideTextWrapper = styled.div`
+  width: 100%;
+  margin-top: 5%;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+`
+
+export const HeaderWrapper = styled.div`
+  z-index: 2;
+  color: #ffffff;
+  font-size: 5rem;
+  text-shadow: 0px 0px 5px #1f1f1f;
+  height: 100%;
+  width: 100vw;
+  padding: 0 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 30%;
+  min-width: 800px;
+`
+
+export const DescriptionWrapper = styled.div`
+  z-index: 2;
+  color: #ffffff;
+  font-size: 2rem;
+  text-shadow: 0px 0px 5px #1f1f1f;
+  height: 100%;
+  width: 100vw;
+  padding: 0 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 17%;
 `
